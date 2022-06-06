ff447192ad3cde9b4040e525ddb86b78e0722b3f
AK Milad <miladahmed1099@gmail.com>
2022-06-06 14:57:48 +0600

    updated

--- js/main.js
+++ js/main.js
@@ -155,7 +155,27 @@ $(function ($) {
             navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
         });
     }
+
+    if ($('#featured-slider-photo').length > 0) {
+        $('#featured-slider-photo').owlCarousel({
+            loop: true,
+            items: 1,
+            dots: false,
+            nav: true,
+            autoplayTimeout: 4000,
+            slideSpeed: 10000,
+            autoplay: true,
+            animateOut: 'fadeOut',
+            autoplayHoverPause: true,
+            mouseDrag: false,
+            touchDrag: false,
+            responsiveClass: true,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
+        });
+    }
     /*========================
+
+    
         featured slider 2
    ==========================*/
     if ($('#featured-slider-2').length > 0) {
@@ -691,3 +711,46 @@ $('.popup-youtube').magnificPopup({
     });
 
 })(jQuery);
+
+
+
+
+
+
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
